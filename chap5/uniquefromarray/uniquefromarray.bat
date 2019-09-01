9051b6c5bc113c0ea87c3db54bbe50c7697ef248
guajiropa <guajiropa@hotmail.com>
2019-09-01 18:42:18 -0400

    Started chapter five

---
+++ chap5/uniquefromarray/uniquefromarray.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET GOPATH=I:\SRC\repos\GO\Hands-OnGoProgramming\chap5\uniquefromarray
+SET GOBIN=I:\SRC\repos\GO\Hands-OnGoProgramming\chap5\uniquefromarray\bin
+ECHO Developmemt environment has been setup for 'uniquefromarray.go'
+ECHO go env
+go env
