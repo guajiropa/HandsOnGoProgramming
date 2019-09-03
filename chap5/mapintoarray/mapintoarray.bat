22d6b8e010ba2c17f4ee1a18889bdabd7dbd79ac
guajiropa <guajiropa@hotmail.com>
2019-09-03 00:07:02 -0400

    Started lesson six of chapter five

---
+++ chap5/mapintoarray/mapintoarray.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET GOPATH=I:\SRC\repos\GO\Hands-OnGoProgramming\chap5\mapintoarray
+SET GOBIN=I:\SRC\repos\GO\Hands-OnGoProgramming\chap5\mapintoarray\bin
+ECHO Development environment had been setup for 'mapintoarry.go'
+ECHO go env
+go env
