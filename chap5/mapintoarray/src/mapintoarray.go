40b271213ae358e6e081c5f7bfeb17e148c1f9fb
guajiropa <guajiropa@hotmail.com>
2019-09-04 11:13:39 -0400

    Complete lesson six of chapter five

---
+++ chap5/mapintoarray/src/mapintoarray.go
@@ -0,0 +1,39 @@
+/*
+** AUTHOR	: Robert James Patterson
+** DATE		: 09/04/2019
+** SYNOPSIS	: we are going to see how to convert a map into an array of keys and values. Let's
+** imagine a variable named nameAges that has map, as shown in the following code block, and we map
+** string values to integer values. There are names and ages too.
+**
+** We need to add a new struct named NameAge and it will have Name as a string and Age as integer.
+** We are going to iterate over our nameAges map now. We'll be using a for loop and when you use a
+** range operator on a map type, it returns two things, a key and the value.
+ */
+package main
+
+import (
+	"fmt"
+)
+
+type NameAge struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	var nameAgeSlice []NameAge
+
+	nameAges := map[string]int{
+		"Michelle": 30,
+		"Jasmin":   25,
+		"Savana":   23,
+		"Vickey":   29,
+		"Jessica":  32,
+	}
+
+	for key, value := range nameAges {
+		nameAgeSlice = append(nameAgeSlice, NameAge{key, value})
+	}
+
+	fmt.Println(nameAgeSlice)
+}
